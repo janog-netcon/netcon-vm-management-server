21bdf4712da94d5d8150f49f23e181f7b60dc17e
nakagawa.ryo <nakagawa.ryo@linecorp.com>
2022-07-13 12:03:12 +0900

    add preemptible false

--- src/client/gce_client.py
+++ src/client/gce_client.py
@@ -70,6 +70,10 @@ def create_instance(machine_image_name, project, zone, user_id, password):
 
     request_body = {
         "name": name,
+        "scheduling":
+            {
+                "preemptible": "false"
+            },
         "sourceMachineImage": "https://www.googleapis.com/compute/v1/projects/" + project + "/global/machineImages/" + machine_image_name,
         "metadata": {
             "kind": "compute#metadata",
